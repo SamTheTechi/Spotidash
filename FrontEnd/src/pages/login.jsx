44b0b003c31231d0571b95d50bd2908c01a66bc3
SamTheProBot <gsameer478@gmail.com>
2024-01-20 01:59:07 +0530

    login page

--- FrontEnd/src/pages/login.jsx
+++ FrontEnd/src/pages/login.jsx
@@ -2,19 +2,25 @@ import React from "react";
 const Login = () => {
   return (
     <>
-      <main className="bg-black  h-screen w-screen flex justify-center items-center">
-        <article className="bg-gray-800 min-w-[70vh] aspect-[1.4/1] flex justify-center text-white items-center flex-col">
-          <section className=" flex items-start justify-start h-[20%]  overflow-hidden">
-            <img
-              src="https://open.spotifycdn.com/cdn/images/favicon32.b64ecc03.png"
-              alt=""
-              className=" p-1 min-h-[70%]"
-            />
-            <h2 className=" p-1">SpotiDash</h2>
+      <main className="bg-black text-white overflow-x-hidden h-screen w-screen  box-border">
+        <header className=" flex justify-start items-start h-[20%] w-[100%] p-5 overflow-hidden">
+          <img
+            src="https://open.spotifycdn.com/cdn/images/favicon32.b64ecc03.png"
+            alt=""
+            className="pr-1 h-[1.6rem]"
+          />
+          <h2 className="items-center text-[1rem] font-semibold">
+            SpotifyDash
+          </h2>
+        </header>
+        <article className="pt-[10vh] flex items-center justify-around flex-col h-[40%]">
+          <section className=" h-[auto] text-3xl font-bold p-5">
+            Login to Countinue <br /> to SpotiDash.
           </section>
-          <section className=" h-[50%] ">Login in to SpotiDash</section>
-          <section className=" h-[30%] ">
-            <button>Log in</button>
+          <section className=" h-[auto]">
+            <button className="text-black font-semibold bg-[#1ed760] min-w-[15rem] aspect-[5/1] rounded-[30px] pt-[-80px]">
+              Log in
+            </button>
           </section>
         </article>
       </main>
