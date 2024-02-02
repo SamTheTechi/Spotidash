dbfa848bd505c37c5bc9df07bb89dd29d32b28df
SamTheProBot <gsameer478@gmail.com>
2024-02-02 11:25:42 +0530

    idk

--- FrontEnd/src/context/Context.js
+++ FrontEnd/src/context/Context.js
@@ -2,6 +2,5 @@ import { createContext } from "react";
 
 const TokenContext = createContext();
 const PlaylistContext = createContext();
-// const userInfo
 
 export { TokenContext, PlaylistContext };
