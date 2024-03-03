c398ef1055700eb352ca8e9d1851ea4fdd591b60
SamTheProBot <gsameer478@gmail.com>
2024-03-03 22:59:07 +0530

    icon Fix

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,16 +1,16 @@
-dimport React, { useContext } from 'react';
+import React, { useContext } from 'react';
 import Login from './pages/login';
 import DashboardComp from './pages/main';
 import { Route, Routes } from 'react-router-dom';
 import './Tailwind.css';
 
 const App = () => {
-  return ( 
-    <div style={{ touchAction: 'none' , overflow : 'hidden' }}>
-    <Routes>
-      <Route path='/' element={<Login />}></Route>
-      <Route path='/dashboard/*' element={<DashboardComp />}></Route>
-    </Routes>
+  return (
+    <div style={{ touchAction: 'none', overflow: 'hidden' }}>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/dashboard/*' element={<DashboardComp />}></Route>
+      </Routes>
     </div>
   );
 };
