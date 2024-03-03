dd21fde3c096d6fe40ac72aa527d8daf195cc375
Sameer Gupta <125123756+SamTheProBot@users.noreply.github.com>
2024-03-03 20:42:23 +0530

    Update App.jsx
    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,15 +1,17 @@
-import React, { useContext } from 'react';
+dimport React, { useContext } from 'react';
 import Login from './pages/login';
 import DashboardComp from './pages/main';
 import { Route, Routes } from 'react-router-dom';
 import './Tailwind.css';
 
 const App = () => {
-  return (
+  return ( 
+    <div style={{ touchAction: 'none' , overflow : 'hidden' }}>
     <Routes>
       <Route path='/' element={<Login />}></Route>
       <Route path='/dashboard/*' element={<DashboardComp />}></Route>
     </Routes>
+    </div>
   );
 };
 
