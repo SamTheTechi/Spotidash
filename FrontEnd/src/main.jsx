c02f684c4b07f99b5ae4c814fbfc92d1e6c6e13a
SamTheProBot <gsameer478@gmail.com>
2024-03-05 02:41:09 +0530

    bug fix
    bug fix

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -4,18 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import TokenContextProvider from './context/tokenContextProvider';
 import UserPlaylistProvider from './context/userPlaylistProvider';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+// import { SpeedInsights } from '@vercel/speed-insights/next';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SpeedInsights>
-      <TokenContextProvider>
-        <UserPlaylistProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </UserPlaylistProvider>
-      </TokenContextProvider>
-    </SpeedInsights>
+    {/* <SpeedInsights> */}
+    <TokenContextProvider>
+      <UserPlaylistProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UserPlaylistProvider>
+    </TokenContextProvider>
+    {/* </SpeedInsights> */}
   </React.StrictMode>
 );
