7465197f2e7da121c027d2d47cde1aba7ec5b3a2
SamTheProBot <gsameer478@gmail.com>
2024-03-05 02:32:20 +0530

    .env update

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import TokenContextProvider from "./context/tokenContextProvider";
-import UserPlaylistProvider from "./context/userPlaylistProvider";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import TokenContextProvider from './context/tokenContextProvider';
+import UserPlaylistProvider from './context/userPlaylistProvider';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <SpeedInsights> */}
-    <TokenContextProvider>
-      <UserPlaylistProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </UserPlaylistProvider>
-    </TokenContextProvider>
-    {/* </SpeedInsights> */}
+    <SpeedInsights>
+      <TokenContextProvider>
+        <UserPlaylistProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </UserPlaylistProvider>
+      </TokenContextProvider>
+    </SpeedInsights>
   </React.StrictMode>
 );
