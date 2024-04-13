896a3122b8441db0a6970c536bb0ab4ee3b3b89e
SamTheProBot <gsameer478@gmail.com>
2024-04-13 15:01:23 +0530

    qparam added
    framer added

--- FrontEnd/src/components/blend.jsx
+++ FrontEnd/src/components/blend.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { FRAMER_FADE_INOUT } from '../util/framer';
 import { PlaylistContext } from '../context/Context';
 
 const Blend = () => {
   return (
     <>
-      <div className='border-[3px] sm:border-[5px]  border-[rgba(0,0,0,0.1)] rounded-[15px]  flex justify-center items-center overflow-hidden sm:m-1.5 m-1 bg-blue-500'>
+      <motion.section
+        {...FRAMER_FADE_INOUT}
+        className='border-[3px] sm:border-[5px]  border-[rgba(0,0,0,0.1)] rounded-[15px]  flex justify-center items-center overflow-hidden sm:m-1.5 m-1 bg-blue-500'>
         <Link to={`blend`}>
           <img
             src={`/Blend.jpg`}
@@ -13,7 +17,7 @@ const Blend = () => {
             className='rounded-[15px] hover:scale-[1.03] opacity-80 hover:opacity-50 transition duration-150 ease-in'
           />
         </Link>
-      </div>
+      </motion.section>
     </>
   );
 };
--- FrontEnd/src/pages/search.jsx
+++ FrontEnd/src/pages/search.jsx
@@ -5,23 +5,20 @@ import { TokenContext, PlaylistContext } from '../context/Context';
 
 const DeepSearch = () => {
   const { token } = useContext(TokenContext);
-  const { userPlaylist } = useContext(PlaylistContext);
-  const [searchparam, setSearchparam] = useState(``);
+  const [searchparam, setSearchparam] = useState(`yours`);
   const [searched, setSearched] = useState();
 
   const handleInput = (e) => {
-    console.log(`aalo`);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(`dagta`);
+    setTimeout(() => {
+      setSearchparam(e.target.value);
+    }, 750);
   };
 
   useEffect(() => {
-    const SearchedSongs = async () => {
+    const handleSubmit = async () => {
       try {
         const data = await axios.get(
-          'https://api.spotify.com/v1/search?q=remaster%2520artist%3AMiles%2520Davis&type=track',
+          `https://api.spotify.com/v1/search?q=${searchparam}&type=track&market=JP`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -33,29 +30,27 @@ const DeepSearch = () => {
         throw e;
       }
     };
-    SearchedSongs();
-  }, [token]);
+    handleSubmit();
+  }, [token, searchparam]);
 
-  console.log(searched);
   return (
     <>
-      <header className=' flex justify-end flex-row lg:h-[10%] w-[100%] pr-6 lg:pt-5 lg:pr-12 overflow-hidden mb-8 sm:mb-12'>
+      <header className=' flex justify-end flex-row lg:h-[10%] w-[100%] pr-6 lg:pt-5 lg:pr-12 overflow-hidden mb-5 sm:mb-0'>
         <Link to={`/dashboard`}>
           <button className='text-black font-semibold bg-custonmGreen hover:bg-custonmGreenHover w-[7rem] lg:w-[9rem] h-[2rem] lg:h-[2.5rem] rounded-[30px] border-[5px] border-[rgba(0,0,0,0.2)]'>
             Go-Back
           </button>
         </Link>
       </header>
-      <div className=' w-[100%] flex justify-center flex-row h-[3%] md:h-[3.5%] mb-5'>
+      <section className=' w-[100%] flex justify-center flex-row h-[3%] md:h-[3.5%] mb-5'>
         <input
+          placeholder='Search for songs....'
           onChange={handleInput}
-          onClick={handleSubmit}
           type='text'
           className='w-[75%] sm:w-[70%] md:w-[65%] lg:w-[60%] xl:w-[45%] h-[100%] rounded-[8px] text-zinc-900 px-2 border-[2px] border-customLightGray'
         />
-      </div>
-      <div className='w-[90%] h-[65%] my-3 px-5 overflow-y-auto overflow-x-hidden rounded-[12px] bg-gray-700'>
-        {/* <div> */}
+      </section>
+      <section className='w-[90%] h-[65%] my-3 p-5 overflow-y-auto overflow-x-hidden rounded-[12px] bg-gray-700'>
         {searched &&
           searched.map((songs) => {
             let imageUrl = songs.album.images.find(
@@ -66,14 +61,13 @@ const DeepSearch = () => {
             let artist = songs.artists.map((artist) => artist.name)?.join(`, `);
             return (
               <Tracks
-                key={songs.id}
                 imageUrl={imageUrl}
                 name={name}
                 preview={preview}
                 artist={artist}></Tracks>
             );
           })}
-      </div>
+      </section>
     </>
   );
 };
@@ -83,15 +77,17 @@ export default DeepSearch;
 const Tracks = ({ imageUrl, name, artist, preview }) => {
   return (
     <>
-      <section>
-        <img src={`${imageUrl}`} alt={name} c />
-        <div>
-          <span>{name}</span>
-          <span>{artist}</span>
+      <article className={`flex py-2`}>
+        <img
+          src={`${imageUrl}`}
+          alt={name}
+          className='`aspect-square mr-2 sm:h-[72px] h-[64px] rounded-[8px] shadow-customShadow'
+        />
+        <div className='flex flex-col p-0 sm:p-1 sm:pl-2 pl-1 justify-center'>
+          <span className='text-base font-semibold sm:text-lg'>{name}</span>
+          <span className='font-thin sm:text-base text-sm'>{artist}</span>
         </div>
-        <button>{preview}</button>
-        <button>+</button>
-      </section>
+      </article>
     </>
   );
 };
